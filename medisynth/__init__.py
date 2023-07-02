487457cf4f40ace50b3ee04e3bdc28b5e700ea8c
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2023-07-02 04:23:10 +0100

    adds medisynth/medisynth.py to call class FetalPlaneDataset(Dataset) in examples/preparing_dataset_fetal_planes.ipynb (#4)

---
+++ medisynth/__init__.py
@@ -0,0 +1,1 @@
+from medisynth.medisynth import FetalPlaneDataset
---
+++ medisynth/medisynth.py
@@ -0,0 +1,62 @@
+import os
+
+import pandas as pd
+from skimage import io
+from torch.utils.data import Dataset
+
+
+class FetalPlaneDataset(Dataset):
+    """Fetal Plane dataset."""
+
+    def __init__(self, root_dir, ref,
+                 plane,
+                 brain_plane=None,
+                 us_machine=None,
+                 operator_number=None,
+                 transform=None
+                 ):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied on a sample.
+            plane: 'Fetal brain'; 'Fetal thorax'; 'Maternal cervix'; 'Fetal femur'; 'Fetal thorax'; 'Other'
+            brain_plane: 'Trans-ventricular'; 'Trans-thalamic'; 'Trans-cerebellum'
+            us_machine: 'Voluson E6';'Voluson S10'
+            operator_number: 'Op. 1'; 'Op. 2'; 'Op. 3';'Other'
+            
+        return image
+        """
+        self.root_dir = root_dir
+        self.ref = pd.read_csv(ref, sep=';')
+        self.ref = self.ref[self.ref['Plane'] == plane]
+        if plane == 'Fetal brain':
+            self.ref = self.ref[self.ref['Brain_plane'] == brain_plane]
+        if us_machine is not None:
+            self.ref = self.ref[self.ref['US_Machine'] == us_machine]
+        if operator_number is not None:
+            self.ref = self.ref[self.ref['Operator'] == operator_number]
+
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.ref)
+
+    def __getitem__(self, idx):
+
+        # print(f'idx: {idx} \n')
+        # print(f'self.ref.iloc[idx, 0]: {self.ref.iloc[idx, 0]} \n')
+
+        img_name = os.path.join(self.root_dir,
+                                self.ref.iloc[idx, 0] + '.png')
+        # print(img_name)
+
+        image = io.imread(img_name)
+
+        # print(type(image))
+        # print(image.dtype)
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image
